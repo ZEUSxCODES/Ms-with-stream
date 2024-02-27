ee33bb7ddfc65b56992bb192480cc29d179101b1
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-02-27 11:53:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get("mongodb+srv://anmol0700:anmol0700@cluster0.fcnpl9j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI',"mongodb+srv://anmol0700:anmol0700@cluster0.fcnpl9j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "RIONETWORKS")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
